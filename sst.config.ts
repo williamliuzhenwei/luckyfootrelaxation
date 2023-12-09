b3673cefb065a02fdcdabfa7a65a463996319dda
williamliuzhenwei <williamliuzhenwei@gmail.com>
2023-12-09 11:12:05 -0800

    Securing API

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,7 @@
 import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
+import { ApiStack } from "./stacks/ApiStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 export default {
   config(_input) {
@@ -9,6 +11,6 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(StorageStack);
+    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
   }
 } satisfies SSTConfig;
